451370b5044a91eeb0be74e4ea71800fded0ba88
Chris <chris-schilders@hotmail.nl>
2021-02-11 17:08:38 +0100

    Updated swift package

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "rasterizer",
+    name: "Rasterizer",
     products: [
         .library(name: "rasterizer", targets: ["rasterizer"])
     ],
