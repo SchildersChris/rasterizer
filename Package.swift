35c9b284bcef6299971f9d6b086b35f81fffd0d2
Chris <chris-schilders@hotmail.nl>
2021-02-11 17:04:30 +0100

    Updated swift package

--- Package.swift
+++ Package.swift
@@ -1,4 +1,15 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let package = Package(name: "Rasterizer")
+let package = Package(
+    name: "rasterizer",
+    products: [
+        .library(name: "rasterizer", targets: ["rasterizer"])
+    ],
+    targets: [
+        .target(
+            name: "rasterizer",
+            path: "./Sources/"
+        )
+    ]
+)
